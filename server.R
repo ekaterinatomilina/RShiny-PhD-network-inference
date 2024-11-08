3805ed34cbca0a2db47c2d179fbe7448dd9890d2
Ekaterina TOMILINA <ekaterina.tomilina@inrae.fr>
2024-11-08 14:56:24 +0100

    new server and ui

--- server.R
+++ server.R
@@ -1,11 +1,206 @@
-# Defini la partie server
-server <- function(input, output, session) {
-
-    output$SK8Image <- renderImage({
-	    list(src= normalizePath(file.path("www/images","SK8.png")),
-		 contentType = 'image/png',
-		 width = 200,
-		 alt = "Logo SK8")
-    }, deleteFile = FALSE)
+server <- function(input, output,session) {
+  # Estimation tab
+  dataframe <- reactive({
+    file <- input$dataset
+    ext <- tools::file_ext(file$datapath)
+    req(file)
+    validate(need(ext == "csv", "Please upload a csv file"))
+    read.csv2(file$datapath, header = TRUE,sep=";")
+  })
+  
+  trunc_data <- reactive({
+    if(!input$subset){
+      dataframe()
+    }else{
+      dataframe()[input$rowmin:input$rowmax,input$colmin:input$colmax]
+    }
+  })
+  
+  output$contents <- renderTable({
+    trunc_data()
+  })
+  
+  output$download_data <- downloadHandler(filename = "new_dataset.csv",
+                                          content = function(file){
+                                            write.csv2(trunc_data(),file)
+                                          }
+  )
+  
+  type_vec <- reactive({
+    if(!input$specify){
+      type <- c()
+      for(i in 1:dim(dataframe())[2]){
+        if(sum(dataframe()[,i]%%1)==0){
+          type <- c(type,"D")
+        }else{
+          type <- c(type,"C")
+        }
+      }
+    }else{
+      type <- c(rep("C",input$cont),rep("D",input$disc))
+    }
+    return(type)
+  })
+  
+  
+  PROGRESS_FILE <- "progress.txt"
+  
+  task <- ExtendedTask$new(function(data,type){
+    mirai({
+      sink(PROGRESS_FILE,type="output")
+      on.exit(sink())
+      rho_estim(data,type,parallel=TRUE)
+    }, rho_estim=rho_estim,data=data,type=type,PROGRESS_FILE = PROGRESS_FILE)
+  })
+  observeEvent(input$dataset, task$invoke(dataframe(),type_vec()))
+  
+  progress <- reactiveFileReader(100, session, PROGRESS_FILE, function(path) {
+    if (file.exists(path)){
+      print(path)
+      print(readLines(path, -1, warn = FALSE))}
+    else{print("")}
+  })
+  output$progress <- renderText({ progress() })
+  
+  observeEvent(task$result(), unlink(PROGRESS_FILE))
+  
+  matrix_estim <- reactive({
+    if(!input$subsetmat){
+      task$result()
+    }else{
+      task$result()[input$matrmin:input$matrmax,input$matcmin:input$matcmax]
+    }
+  })
+  
+  
+  output$Mhat <- renderTable({
+    matrix_estim()
+  })
+  
+  output$download_matrix <- downloadHandler(filename = "correlation_matrix.csv",
+                                            content = function(file){
+                                              write.csv2(matrix_estim(),file)
+                                            }
+  )
+  
+  
+  cor_plot <- function(){
+    corrplot(matrix_estim(),type="upper",method=input$corrplot_type)
+  }
+  
+  output$corr_plot <- renderPlot({
+    cor_plot()
+  })
+  
+  output$download_corrplot <- downloadHandler(filename = "corrplot.png",
+                                              content = function(file){
+                                                png(file)
+                                                cor_plot()
+                                                dev.off()
+                                              }
+  )
+  
+  observeEvent(input$other_group,
+               {
+                 if(input$other_group){
+                   shinyjs::hide(id="colcont")
+                   shinyjs::hide(id="coldisc")
+                 }else{
+                   shinyjs::show(id="colcont")
+                   shinyjs::show(id="coldisc")
+                 }
+               })
+  
+  new_groups <- reactive({
+    input$nbgps
+  })
+  
+  
+  output$groups1 <- renderUI({
+    output = tagList()
+    for(i in 1:new_groups()){
+      output[[i]] = tagList()
+      output[[i]][[1]]=colourpicker::colourInput(inputId = paste0("colorgp", i), label = paste0("Select the color for group ",i))
+    }
+    output
+  })
+  
+  output$groups3 <- renderUI({
+    output = tagList()
+    for(i in 1:new_groups()){
+      output[[i]] = tagList()
+      output[[i]][[1]]=textInput(inputId = paste0("numgp", i), label = paste0("Select the columns for group ",i))
+    }
+    output
+  })
+  
+  output$groups2 <- renderUI({
+    output = tagList()
+    for(i in 1:new_groups()){
+      output[[i]] = tagList()
+      output[[i]][[1]]=textInput(inputId = paste0("labelgp", i), label = paste0("Enter the label for group ",i))
+    }
+    output
+  })
+  
+  color <- reactive({
+    col <- rep(NA,length(type_vec()))
+    col_order <- c()
+    if(!input$custom){
+      col[which(type_vec()=="C")] <- "goldenrod1"
+      col[which(type_vec()=="D")] <- "lightskyblue"
+      col_order <- c("goldenrod1","lightskyblue")
+    }else if(!input$other_group){
+      col[which(type_vec()=="C")] <- input$colcont
+      col[which(type_vec()=="D")] <- input$coldisc
+      col_order <- c(input$colcont,input$coldisc)
+    }else{
+      col_order <- c()
+      for(i in 1:new_groups()){
+        id <- paste0("numgp",i)
+        color_new <- paste0("colorgp",i)
+        col[as.numeric(unlist(strsplit(input[[id]],",")))] <- input[[color_new]]
+        col_order <-c(col_order,input[[color_new]])
+      }
+    }
+    print(col)
+    return(list(col,col_order))
+  })
+  
+  legendgph <- reactive({
+    if(!input$custom){
+      legend <- c("Continuous","Discrete")
+    }else if(!input$other_group){
+      legend <- c("Continuous","Discrete")
+    }else{
+      legend <- c()
+      for(i in 1:new_groups()){
+        labelgp <- paste0("labelgp",i)
+        legend <- c(legend,input[[labelgp]])
+      }
+      print(legend)
+    }
+    return(legend)
+  })
+  
+  network_ref_estim <- reactive(graph_from_adjacency_matrix(matrix_cor_ts(round(matrix_estim(),3), input$T_hat), mode="undirected", diag=F))
+  
+  plot_hat <- function(){
+    par(xpd=TRUE)
+    plot(network_ref_estim(),vertex.color=color()[[1]])
+    legend("bottomright",inset=c(-0,-0.15), legendgph(),fill=color()[[2]])
+  }
+  
+  output$plot <- renderPlot(plot_hat())
+  
+  output$download_plot <- downloadHandler(filename = "graph.png",
+                                          content = function(file){
+                                            png(file)
+                                            plot_hat()
+                                            dev.off()
+                                          }
+  )
+  
 }
+
 
--- ui.R
+++ ui.R
@@ -1,18 +1,77 @@
-# Definition de la partie UI 
-ui <- fluidPage(
-
-    # Titre de l application
-    titlePanel("Application de Dev - SK8-Team"),
-
-    # affiche une image
-    mainPanel(
-        imageOutput("SK8Image")
-    ),
-    div(
-        class="footer",
-        includeHTML("footer.html"),
-        br(),
-        HTML(paste0('<div style="bottom:0;font-size:12px;position: absolute;right: 0;"> Version ',{Sys.getenv("APP_VERSION")},' </div>'))
-    )
+ui <- fluidPage(theme=bs_theme(version=5,bootswatch = "minty"),
+                useShinyjs(),
+                # Application title
+                titlePanel("Gaussian copula correlation network inference"),
+                card(tags$div("This application enables the user to perform network inference on heterogeneous variables by assuming a Gaussian copula structure.
+    Even if the data set is high dimensional, the computational cost is reduced by using a pairwise likelihood. The calculations are performed via the ",
+                              tags$a(href="https://cran.r-project.org/web/packages/heterocop/index.html","heterocop"),
+                              "R package.")),
+                sidebarLayout(
+                  sidebarPanel(
+                    card(strong("Load your own data"), fileInput("dataset","Please use .csv format, with variables separated by a semi colon",accept=".csv"),
+                         em("The application can automatically detect your variable type (continuous or  discrete), but we do not guarantee full reliability. 
+                             You can check the box below to specify the number of variables of each type yourself. In this case, note that the 
+                             columns of your .csv have to contain all continuous variables first.")
+                    )
+                    ,
+                    card("", checkboxInput("specify","Specify the number of each variables",value=FALSE),
+                         conditionalPanel(
+                           condition = "input.specify",
+                           numericInput('cont', 'How many continuous variables ?', value=0),
+                           numericInput('disc', 'How many discrete variables ?', value=0)
+                         )
+                    )
+                  ),
+                  mainPanel(
+                    accordion(
+                      accordion_panel("Dataset",
+                                      checkboxInput("subset","Display only a subset of your data",value=FALSE),
+                                      conditionalPanel( condition = "input.subset",
+                                                        card("Which rows and columns do you want to display?",
+                                                             fluidRow("Columns"),
+                                                             fluidRow(column(5,numericInput('colmin',label="from",value=1,min=1)),
+                                                                      column(5,numericInput('colmax',label="to",value=1,min=1))),
+                                                             "Rows",
+                                                             fluidRow(column(5,numericInput('rowmin',label="from",value=1,min=1)),
+                                                                      column(5,numericInput('rowmax',label = "to",value=1,min=1))))
+                                      ),
+                                      card("",tableOutput("contents"))
+                                      ,downloadButton("download_data", label="Download the selected dataset")),
+                      accordion_panel("Correlation matrix",
+                                      checkboxInput("subsetmat","Display only a subset of your correlation matrix",value=FALSE),
+                                      conditionalPanel( condition = "input.subsetmat",
+                                                        card("Which rows and columns do you want to display?",
+                                                             fluidRow("Columns"),
+                                                             fluidRow(column(5,numericInput('matcmin',label="from",value=1,min=1)),
+                                                                      column(5,numericInput('matcmax',label="to",value=1,min=1))),
+                                                             "Rows",
+                                                             fluidRow(column(5,numericInput('matrmin',label="from",value=1,min=1)),
+                                                                      column(5,numericInput('matrmax',label = "to",value=1,min=1))))
+                                      ),
+                                      card(textOutput("progress"),
+                                           tableOutput("Mhat"))
+                                      ,downloadButton("download_matrix", label="Download the correlation matrix of the copula")),
+                      accordion_panel("Corrplot",
+                                      selectInput("corrplot_type",label="Customize your corrplot",choices=list("circle", "square", "ellipse", "number", "shade", "color", "pie")),
+                                      plotOutput("corr_plot"),
+                                      downloadButton("download_corrplot", label="Download the corrplot")),
+                      accordion_panel("Graphical representation",
+                                      em("The graph corresponding to the correlation matrix is shown below. An edge is present between two vertices if their correlation coefficient is greater than the threshold defined below. The colors correspond to the variable type (discrete or continuous), but more coloring options are available in the Customize your graph section."),
+                                      numericInput("T_hat",label = "Enter the correlation threshold", value = 0.5, min = 0,step=0.1),
+                                      checkboxInput("custom","Customize your graph",value=FALSE),
+                                      conditionalPanel(condition="input.custom",
+                                                       fluidRow(column(5,colourpicker::colourInput("colcont",label="Select the color for continuous variables",value="goldenrod1")),
+                                                                column(5,colourpicker::colourInput("coldisc",label="Select the color for discrete variables",value="lightskyblue"))),
+                                                       checkboxInput("other_group","I want to add more groups of variables",value=F),
+                                                       conditionalPanel(condition="input.other_group",
+                                                                        numericInput("nbgps","Enter the number of groups",value=2,min=1),
+                                                                        fluidRow(column(4,uiOutput("groups1")),column(4,uiOutput("groups2")),column(4,uiOutput("groups3")))
+                                                       )),
+                                      fluidRow(plotOutput("plot"),
+                                               downloadButton("download_plot", label="Download the graph"))
+                      )
+                    )
+                  )
+                )
 )
 
